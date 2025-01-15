b2e9cdf0db0304fd236dd9e1a7e41e07dd761727
SG-Repo2 <sgroebe10@gmail.com>
2025-01-15 14:24:09 -0600

    portal init

--- .next/static/webpack/webpack.1f0990efb7382c06.hot-update.js
+++ .next/static/webpack/webpack.1f0990efb7382c06.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "2d17a2252daac503"; }
+/******/ 	__webpack_require__.h = function() { return "502a524fe637fef2"; }
 /******/ }();
 /******/ 
 /******/ }
